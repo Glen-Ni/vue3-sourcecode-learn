201e31522b7c714f2f25e8662a0969c675cf5e3a
小僵尸RUA <nijialun888@hotmail.com>
2021-07-31 15:17:00 +0800

    get方法，懒递归

--- packages/reactivity/dist/reactivity.global.js
+++ packages/reactivity/dist/reactivity.global.js
@@ -6,23 +6,41 @@ var VueReactivity = (function (exports) {
   };
 
   function createGetter(isReadonly, shallow) {
+      if (isReadonly === void 0) { isReadonly = false; }
       if (shallow === void 0) { shallow = false; }
+      /**
+       * target 原来对象
+       * key 取啥属性
+       * recevier 代理对象
+       */
       return function get(target, key, receiver) {
-          if (isObject(target[key]) && !shallow) {
-              return reactive(target[key]);
+          // if( isObject(target[key]) && !shallow) {
+          //   return reactive(target[key]);
+          // }
+          console.log('【读取了】', key);
+          var res = Reflect.get(target, key, receiver);
+          if (!isReadonly) {
+              console.log('读取，收集依赖更新视图', key);
           }
-          console.log('读取了', key);
-          return target[key];
+          if (shallow) {
+              return res;
+          }
+          if (isObject(res)) {
+              return isReadonly ? readonly(res) : reactive(res);
+          }
+          return res;
       };
   }
   function createSetter(isShallow) {
       return function set(target, key, value, receiver) {
-          console.log(key + "\u7684set\u8C03\u7528\u4E86, \u7B49\u5F85\u89C6\u56FE\u66F4\u65B0");
+          var res = Reflect.set(target, key, value, receiver);
+          console.log("\u3010\u8BBE\u7F6E\u4E86\u3011" + key + "\u7684set\u8C03\u7528\u4E86, \u7B49\u5F85\u89C6\u56FE\u66F4\u65B0");
+          return res;
       };
   }
   var get = createGetter();
   var shallowGet = createGetter(false, true);
-  var readonlyGet = createGetter();
+  var readonlyGet = createGetter(true);
   var shallowReadonlyGet = createGetter(true, true);
   var set = createSetter();
   var shallowSet = createSetter();
--- packages/reactivity/src/baseHandler.ts
+++ packages/reactivity/src/baseHandler.ts
@@ -1,19 +1,36 @@
 import {isObject} from "@vue/shared";
-import {reactive} from "./reactive";
+import {reactive, readonly} from "./reactive";
 
 function createGetter(isReadonly = false, shallow = false,) {
+  /**
+   * target 原来对象
+   * key 取啥属性
+   * recevier 代理对象
+   */
   return function get(target, key, receiver) {
-    if( isObject(target[key]) && !shallow) {
-      return reactive(target[key]);
+    // if( isObject(target[key]) && !shallow) {
+    //   return reactive(target[key]);
+    // }
+    console.log('【读取了】', key)
+    const res = Reflect.get(target, key, receiver)
+    if(!isReadonly) {
+      console.log('读取，收集依赖更新视图', key);
     }
-    console.log('读取了', key)
-    return target[key]
+    if (shallow) {
+      return res;
+    }
+    if (isObject(res)) {
+      return isReadonly ? readonly(res) : reactive(res);
+    }
+    return res;
   }
 }
 
-function createSetter(isShallow= false) {
-  return function set(target, key,value, receiver) {
-    console.log(`${key}的set调用了, 等待视图更新`)
+function createSetter(isShallow = false) {
+  return function set(target, key, value, receiver) {
+    const res = Reflect.set(target, key, value, receiver)
+    console.log(`【设置了】${key}的set调用了, 等待视图更新`)
+    return res
   }
 }
 
